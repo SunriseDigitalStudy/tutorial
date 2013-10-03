0fb61ec91330ca1444d1f9051ae68e9849d0840c
Masamoto Miyata <miyata@sincere-co.com>
2013-10-03 14:50:35 +0900

    並び替えを実装。

--- board.sunrisedigital.jp/app/controllers/default/ControlController.php
+++ board.sunrisedigital.jp/app/controllers/default/ControlController.php
@@ -3,12 +3,9 @@
 
 class ControlController extends Sdx_Controller_Action_Http
 {
-	public function threadAction()
-	{
-		$this->_helper->scaffold->setViewRendererPath('default/control/scaffold.tpl');
-		$this->_helper->scaffold->run();
-	}
-
-  
-
+  public function __call($name, $arguments)
+  {
+    $this->_helper->scaffold->setViewRendererPath('default/control/scaffold.tpl');
+    $this->_helper->scaffold->run();
+  }
 }
--- board.sunrisedigital.jp/app/views/default/control/scaffold.tpl
+++ board.sunrisedigital.jp/app/views/default/control/scaffold.tpl
@@ -2,6 +2,11 @@
 
 {block title append} {$page_name}{/block}
 
+{block js append}
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+{include 'sdx/include/scaffold/js.tpl'}
+{/block}
+
 {block css append}
 <link rel="stylesheet" type="text/css" href="/css/sdx/scaffold.bootstrap.css">
 {/block}
