8ba0f3557eab67b6c6b96946a49702da62dfb843
Masamoto Miyata <miyata@sincere-co.com>
2013-10-03 18:36:16 +0900

    Groupingmまで実装。

--- board.sunrisedigital.jp/app/controllers/default/ControlController.php
+++ board.sunrisedigital.jp/app/controllers/default/ControlController.php
@@ -5,6 +5,7 @@ class ControlController extends Sdx_Controller_Action_Http
 {
   public function __call($name, $arguments)
   {
+    $this->_getContext()->enableNoCacheMode();
     $this->_helper->scaffold->setViewRendererPath('default/control/scaffold.tpl');
     $this->_helper->scaffold->run();
   }
